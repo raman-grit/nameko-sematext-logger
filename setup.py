595786c858501cfc4d861880156cf81ded167f6f
ramands <raman.damodar@healthify.co>
2020-03-18 22:58:03 +0530

    version change

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="nameko-sematext-logger",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(exclude=["test", "test.*"]),
     description="Nameko extension for sematext logging",
     long_description=open("README.md").read(),
